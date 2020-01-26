a3d8cca12b2eaef6b68a47a29023b063a6f1a209
Laurent Bendavid <laurent.bendavid@gmail.com>
2020-01-26 18:16:02 +0100

    updates but bug not found...

--- script/acbegpx2garmin.py
+++ script/acbegpx2garmin.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # -*-coding:Utf-8 -*
 
-
 #import tarfile
 #import zipfile
 #from datetime import datetime
@@ -21,6 +20,8 @@
 Ce module contient notre programme
 """
 
+import requests
+import logging
 import os
 import os.path
 import re
@@ -29,6 +30,49 @@ import urllib.request
 import sys
 import re
 import shutil
+import time
+
+"""
+Functions
+"""
+def switch_re(patterns, text):
+    """
+    Find re matching a text. To simulate a SWITCH test
+    Input (text), check to each regexp, il match return result
+    If nothing, None
+
+    Args:
+        patterns (hash) : hash of regexp => result
+        text : regexp
+    
+    Return:
+        result associated with one regexp
+    
+    """
+    for regexp, result in patterns.items():
+        if regexp.match(text):
+            return result
+    return None
+
+def eject_usb_device(device):
+    """
+    Eject one usb device
+    Note:
+        the full path is concatenate in the function. Use logging.
+    
+    Args:
+        device (str) : usb name device to eject.
+        
+    Returns:
+        None
+    
+    Raises:
+        None. Propagate erros from subprocess
+    """
+    device = os.path.join('/dev', device)
+    logging.info(device)
+    subprocess.run(['udisksctl', 'unmount', '-b', device])
+    subprocess.run(['udisksctl', 'power-off', '-b', device])
 
 """
 Variables importantes / valeur par défaut
@@ -40,6 +84,35 @@ download_dir = os.path.join(os.path.expanduser("~"), "Downloads", "gpx")
 """
 Template expression régulière
 """
+
+re_tag            = re.compile(r"""
+        <td[^>]*prochain[^>]*>\w+\s+(\d+)\s+(\S+)\s+(\d+)</td><td[^>]*>([\dh]+).*? # la date
+        <a\s+href="                                                                # le starter de lien
+        (\S+openrunner[^"]+)                                                       # le lien openrunner
+        "[^>]*>                                                                    # la fin du de la balise de lien
+        <img\s+title="Open\w+\s+                                                   # l'image avec un titre
+        ([^"]+)""", re.VERBOSE | re.MULTILINE | re.DOTALL)
+
+
+
+re_replace_track = re.compile(r'<name>(\d+)-([^<]+)</name>')
+"""
+Fin template expression régulière
+"""
+
+re_id = re.compile(r'/(\d+)$')
+def openrunner_id_from_url(url):
+    """
+    Args:
+        url formed like openrunner track
+    Rerturns:
+        id in the url
+    Raises:
+        AttributeError
+    """
+    id = re_id.search(url).group(1)
+    return id
+
 patterns = {
     r'^ja':   '01',
     r'^f':    '02', 
@@ -55,86 +128,112 @@ patterns = {
     r'^d':    '12',
 }
 patterns_compiled = { re.compile(p, re.I): v for p, v in patterns.items() }
+def def_track_name_from_content(item, id):
+    """
+    Args:
+        item : list
+        url  : id of the track
+    Returns:
+        track_name
+    """
+    # Unpack list
+    (day, month, year, start_time, url_openrunner, name) = item
+    # Find month number from month name
+    month = switch_re(patterns_compiled, month)
+    id    = openrunner_id_from_url(url_openrunner)
+    return (name, id, "{}_{}_{}-{}".format(name, id, day, month))
 
-re_tag            = re.compile(r"""
-        <td[^>]*prochain[^>]*>\w+\s+(\d+)\s+(\S+)\s+(\d+)</td><td[^>]*>([\dh]+).*? # la date
-        <a\s+href="                                                                # le starter de lien
-        (\S+openrunner[^"]+)                                                       # le lien openrunner
-        "[^>]*>                                                                    # la fin du de la balise de lien
-        <img\s+title="Open\w+\s+                                                   # l'image avec un titre
-        ([^"]+)""", re.VERBOSE | re.MULTILINE | re.DOTALL)
+def download_openrunner_track_id(id, gpx_local_file):
+    """
+    Args:
+        id (int): openrunner id
+        gpx_local_file(path) : local path
+    Returns:
+        None
+    """
+    gpx_track_url = "https://www.openrunner.com/route/{}/gpx?type=0".format(id)
+    logging.debug("Downloading {} in file '{}'...".format(gpx_track_url, gpx_local_file))
+    urllib.request.urlretrieve(gpx_track_url, gpx_local_file)
 
-re_id             = re.compile(r'/(\d+)$')
-
-re_replace_track = re.compile(r'<name>(\d+)-([^<]+)</name>')
-"""
-Fin template expression régulière
-"""
+def find_and_set_garmin_dir():
+    """
+    Args:
+        None
+    Returns:
+        garmin_dir if garmin connected (could be used as flag)
+    """
+    garmin_inside_path  = ['Garmin', 'NewFiles']
+    garmin_dir          = ""
+    device              = ""
+    
+    reg          = re.compile(r'^(\w+).*?disk (/media/\S+)', re.MULTILINE)
+    try:
+        output     = subprocess.check_output(['lsblk']).decode('utf8')
+        logging.debug(output)
+        
+        (device, garmin_mount) = reg.search(output).group(1,2)
+        garmin_dir = os.path.join(garmin_mount, *garmin_inside_path)
+        
+        if not os.path.isdir(garmin_dir):
+            # Unset garmin dir
+            garmin_dir = ""
+            raise NameError("Garmin dir {} not found.".format(garmin_dir))
+        
+        print("Garmin connected.")
+        
+    except NameError as err:
+        logging.error("{}".format(err))
+    except AttributeError as err:
+        logging.error("no USB Garmin devices")
+    except OSError as err:
+        logging.error("OS Error: {}".format(err))
+    
+    return (device, garmin_dir)
 
 if __name__ == '__main__':
 #
 # Fonction
 #
-    def eject_usb_device(device):
-        device = os.path.join('/dev', device)
-        print(device)
-        subprocess.run(['udisksctl', 'unmount', '-b', device])
-        subprocess.run(['udisksctl', 'power-off', '-b', device])
-
-    def warn(message):
-        print(message, file=sys.stderr)
-
-    def switch_re(patterns, text):
-        for regexp, result in patterns.items():
-            if regexp.match(text):
-                return result
-
     try:
         os.mkdir(download_dir)
-    except:
+    except OSError:
         pass
 
     print("Finding GARMIN...")
-    reg          = re.compile(r'^(\w+).*?disk (/media/\S+)', re.MULTILINE)
-    output       = subprocess.check_output(['lsblk']).decode('utf8')
-    (device, garmin_mount) = reg.search(output).group(1,2)
-    garmin_dir   = os.path.join(garmin_mount, 'Garmin', 'NewFiles')
-    flag_garmin  = False
-    if os.path.isdir(garmin_dir):
-        print("Found GARMIN")
-        flag_garmin = True
+    device, garmin_dir = find_and_set_garmin_dir()
 
-    print("Retrieving url {}...".format(url))
-    page    = urllib.request.urlopen(url)
-    content = page.read().decode(code_page)
+    print("Get url {}...".format(url))
+    try:
+        content = requests.get(url).text
+    except:
+        logging.fatal("Unable to get url {}".format(url))
+        sys.exit(8)
 
-    print("Finding track...")
+    print("Finding track for openrunner {}...".format(id))
     for item in re_tag.findall(content):
-        (day, month, year, start_time, url, name) = item
-        month      = switch_re(patterns_compiled, month)
-        id         = re_id.search(url).group(1)
-        track_name = "{}_{}_{}-{}".format(name, id, day, month)
+        name, id, track_name = def_track_name_from_content(item)
         print("Found track {}".format(track_name))
-    
         gpx_file = os.path.join(download_dir, "{}_{}.gpx".format(name, id))
 
         if not os.path.isfile(gpx_file):
-            print("Downloading in file '{}'...".format(gpx_file))
-            gpx_track_url = "https://www.openrunner.com/route/{}/gpx?type=0".format(id)
-            urllib.request.urlretrieve(gpx_track_url, gpx_file)
+            download_openrunner_track_id(id, gpx_file)
+        else:
+            print("/!\ {} already downloaded".format(os.path.basename(gpx_file)))
     
         tmp_file = os.path.join("/tmp", "{}.gpx".format(track_name))
+        logging.warn("File {} renamed".format(tmp_file))
         with open(tmp_file, 'w') as gpx_dest:
             with open(gpx_file, 'r') as gpx_src:
                 for line in gpx_src:
                     if re_replace_track.search(line):
                         line = re_replace_track.sub(r'<name>\2-\1_{}-{}</name>'.format(day, month), line)
-
                     gpx_dest.write(line)
-        if flag_garmin:
+        
+        if garmin_dir:
             target = os.path.join(garmin_dir, os.path.basename(tmp_file))
-            print(target)
-            shutil.move(tmp_file, os.path.join(garmin_dir, os.path.basename(tmp_file)))
+            print("GARMIN Destination file: {}".format(target))
+            shutil.move(tmp_file, target)
+            time.sleep(10)
 
-    if flag_garmin:
+    if device:
         eject_usb_device(device)
